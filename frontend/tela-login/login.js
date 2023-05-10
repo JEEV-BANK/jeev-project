1c21527707ea4231b6d459d12e7979213be999e6
Erick Alan0123 <erickalan068@gmail.com>
2023-05-09 23:57:45 -0300

    branch/erick: redirecionando ao logar para a tela principal

--- frontend/tela-login/login.js
+++ frontend/tela-login/login.js
@@ -1,6 +1,5 @@
 
 const findUserByLogin = async(req) =>{
-    let aux;
     await fetch(`http://localhost:8081/find-conta-bancaria/${req[0]}/${req[1]}/${req[2]}`, {
         method: 'GET'
     })
@@ -22,30 +21,44 @@ function entrar() {
         conta.value,
         senha.value
     ]
-    findUserByLogin(req);
+    if(agencia.value && conta.value && senha.value){
+        localStorage.clear();
+        findUserByLogin(req);
+    }else {
+        alert('Todos os campo sao obrigatorios')
+    }
 }
 
 function validationLogin(dado) {
     if(dado.length == 0)
         alert('Usuário não encontrado!')
     else {
-        console.log(dado);
         dado.forEach(element => {
-           localStorage.setItem('chave_pix', element.chave_pix);
-           localStorage.setItem('cpf_cnpj', element.cpf_cnpj);
-           localStorage.setItem('dt_nascimento', element.dt_nascimento);
-           localStorage.setItem('email', element.email);
-           localStorage.setItem('id_conta_bancaria', element.id_conta_bancaria);
-           localStorage.setItem('id_usuario', element.id_usuario);
-           localStorage.setItem('instituicao', element.instituicao);
-           localStorage.setItem('nome', element.nome);
-           localStorage.setItem('nome_social', element.nome_social);
-           localStorage.setItem('nr_agencia', element.nr_agencia);
-           localStorage.setItem('nr_conta', element.nr_conta);
-           localStorage.setItem('senha', element.senha);
-           localStorage.setItem('sexo', element.sexo);
-           localStorage.setItem('vl_poupança', element.vl_poupança);
-           localStorage.setItem('vl_saldo', element.vl_saldo);
+            localStorage.setItem('chave_pix', element.chave_pix);
+            localStorage.setItem('cpf_cnpj', element.cpf_cnpj);
+            localStorage.setItem('dt_nascimento', element.dt_nascimento);
+            localStorage.setItem('email', element.email);
+            localStorage.setItem('id_conta_bancaria', element.id_conta_bancaria);
+            localStorage.setItem('id_usuario', element.id_usuario);
+            localStorage.setItem('instituicao', element.instituicao);
+            localStorage.setItem('nome', element.nome);
+            localStorage.setItem('nome_social', element.nome_social);
+            localStorage.setItem('nr_agencia', element.nr_agencia);
+            localStorage.setItem('nr_conta', element.nr_conta);
+            localStorage.setItem('senha', element.senha);
+            localStorage.setItem('sexo', element.sexo);
+            localStorage.setItem('vl_poupança', element.vl_poupança);
+            localStorage.setItem('vl_saldo', element.vl_saldo);
+            isLogado()
         });
     }
 }
+
+function isLogado() {
+    location.reload;
+    if(localStorage.length > 0) {
+        window.location.pathname = "../frontend/tela-principal/tela-principal.html"
+    }
+}
+
+isLogado()
